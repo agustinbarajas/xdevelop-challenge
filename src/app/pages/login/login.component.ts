60441cbef49058f2cf876654edbe9d85e29fb6ae
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-02-23 10:54:58 -0700

    Add login design

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,23 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private readonly fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.email, Validators.required]],
+      password: ['', Validators.required],
+    });
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      // TODO: Implement service to login
+    }
+  }
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,11 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { LoginComponent } from './login/login.component';
 import { PagesRoutingModule } from './pages-routing.module';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, PagesRoutingModule],
+  imports: [
+    CommonModule,
+    PagesRoutingModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+  ],
 })
 export class PagesModule {}
