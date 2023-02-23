64171f9b9fec6a6118b25f373e917f668cc7d82a
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-02-23 11:49:46 -0700

    Add service integration for login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+
+import { UserService } from './../../services/user/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
 })
-export class LoginComponent {
+export class LoginComponent implements OnDestroy {
   loginForm: FormGroup;
+  private readonly unsubscribe$ = new Subject();
 
-  constructor(private readonly fb: FormBuilder) {
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly userService: UserService,
+    private readonly router: Router
+  ) {
     this.loginForm = this.fb.group({
       username: ['', [Validators.email, Validators.required]],
       password: ['', Validators.required],
@@ -17,7 +26,19 @@ export class LoginComponent {
 
   login() {
     if (this.loginForm.valid) {
-      // TODO: Implement service to login
+      const { username, password } = this.loginForm.value;
+
+      this.userService
+        .login(username, password)
+        .pipe(takeUntil(this.unsubscribe$))
+        .subscribe(() => {
+          this.router.navigate(['/'], { replaceUrl: true });
+        });
     }
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next(true);
+    this.unsubscribe$.complete();
   }
 }
