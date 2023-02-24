4ecb49b0304b16786b0cabfc884608b52cd0c639
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-02-23 20:29:19 -0700

    Add signup page design

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,8 +4,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatStepperModule } from '@angular/material/stepper';
 
 import { LoginComponent } from './login/login.component';
 import { PagesRoutingModule } from './pages-routing.module';
@@ -21,6 +25,10 @@ import { SignupComponent } from './signup/signup.component';
     MatFormFieldModule,
     MatButtonModule,
     MatInputModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
   ],
 })
 export class PagesModule {}
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,7 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+import { createSignupForm } from 'src/app/utils/createSignupForm.helper';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  private _signupForm: FormGroup;
+
+  get personalForm(): FormGroup {
+    return this._signupForm.controls['personalForm'] as FormGroup;
+  }
+  get companyForm(): FormGroup {
+    return this._signupForm.controls['companyForm'] as FormGroup;
+  }
+  get addressForm(): FormGroup {
+    return this._signupForm.controls['addressForm'] as FormGroup;
+  }
+
+  constructor(private readonly fb: FormBuilder) {
+    this._signupForm = createSignupForm(this.fb);
+  }
+
+  signup() {
+    if (this._signupForm.valid) {
+      // TODO: Service integration for signup
+    }
+  }
+}
