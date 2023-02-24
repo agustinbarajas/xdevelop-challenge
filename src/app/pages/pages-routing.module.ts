dd4c5a775b1e1748b4466de1efa27965ca43e901
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-02-24 12:03:01 -0700

    Add home route

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'signup',
     component: SignupComponent,
+  },
+  {
+    path: '',
+    component: HomeComponent,
   },
 ];
 
