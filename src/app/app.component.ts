9a474cf9258b4bf7e7a9ec6ae2eae9ab50edb497
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-02-24 14:31:39 -0700

    Add redirection when has or not session

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+
 import { UserService } from './services/user/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent implements OnInit {
-  constructor(private readonly userService: UserService) {}
+export class AppComponent implements OnInit, OnDestroy {
+  private readonly unsubscribe$ = new Subject();
+
+  constructor(
+    private readonly router: Router,
+    private readonly userService: UserService
+  ) {
+    this.userService.hasSession$
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((hasLogin) => {
+        if (hasLogin) {
+          this.router.navigate(['/']);
+        } else {
+          this.router.navigate(['/login']);
+        }
+      });
+  }
 
   ngOnInit() {
     const hasLogin = this.userService.hasLogin();
     this.userService.hasSession$.next(hasLogin);
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next(false);
+    this.unsubscribe$.complete();
   }
 }
