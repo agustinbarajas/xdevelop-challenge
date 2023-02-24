5cf38bb9e680907df8d4cd807ff62984e5b5f32f
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-02-24 14:22:25 -0700

    Add validation to show header buttons

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
 
+import { UserService } from 'src/app/services/user/user.service';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  hasLogin = false;
+
+  constructor(private readonly userService: UserService) {
+    this.userService.hasSession$.subscribe((hasLogin: boolean) => {
+      this.hasLogin = hasLogin;
+    });
+  }
+}
