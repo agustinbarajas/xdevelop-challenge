fd336306a96d1d45e0c0ee0340b55175f17c0fac
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-02-24 20:38:09 -0700

    Fix sanitize inner html

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Observable } from 'rxjs';
 
 import { AwardsService } from 'src/app/services/awards/awards.service';
@@ -12,9 +13,16 @@ export class HomeComponent {
   categories$!: Observable<any>;
   judges$!: Observable<any>;
 
-  constructor(private readonly awardsService: AwardsService) {
+  constructor(
+    private readonly sanitizer: DomSanitizer,
+    private readonly awardsService: AwardsService
+  ) {
     this.about$ = this.awardsService.getAboutGoulaAwards();
     this.categories$ = this.awardsService.getGoulaAwardsCategories();
     this.judges$ = this.awardsService.getGoulaAwardsJudges();
+  }
+
+  sanitizeHtml(html: string) {
+    return this.sanitizer.bypassSecurityTrustHtml(html);
   }
 }
